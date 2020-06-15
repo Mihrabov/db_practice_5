54154c928bef746e8a694e881929f90c674d1cf3
Mihrabov <59070681+Mihrabov@users.noreply.github.com>
2020-06-15 16:39:12 +0300

    Update task_5.sql

--- task_5.sql
+++ task_5.sql
@@ -1,56 +1,45 @@
 #Запрос к 2 заданию    
-SELECT * FROM db_practice_5.cars;
 SELECT nameclient
 FROM cars 
 WHERE Nameclient IN (SELECT Nameclient FROM cars GROUP BY Nameclient HAVING COUNT(*) > 1)
 ORDER BY Nameclient DESC
+
   #Запрос к 3 заданию  
- SELECT * FROM db_practice_5.cars;
-SELECT
-    managersurname,
-    COUNT(managersurname) AS sales
-FROM
-   cars
-GROUP BY
-    Managersurname
-HAVING 
-    sales > 10 and managersurname like '_%'
+SELECT managersurname,
+COUNT(managersurname) AS sales
+FROM cars
+GROUP BY Managersurname
+HAVING sales > 10 and managersurname like '_%'
+  
   #Запрос к 4 заданию  
- SELECT * FROM db_practice_5.cars;
-SELECT
-    managersurname,
-    COUNT(managersurname) AS sales
-FROM
-   cars
-GROUP BY
-    Managersurname
-HAVING 
-    sales < 10 and managersurname like '_%'
+SELECT managersurname,
+COUNT(managersurname) AS sales
+FROM cars
+GROUP BY Managersurname
+HAVING sales < 10 and managersurname like '_%' 
+   
   #Запрос к 5 заданию  
- SELECT  FROM db_practice_5.cars;
 SELECT typepayment, nameclient
 From cars
 WHERE typepayment = 'безналичная';
+
   #Запрос к 6 заданию  
-SELECT * FROM db_practice_5.cars;
 SELECT mark, Model, Nameclient, datesales
 From cars 
 Where Nameclient = 'Якушев Никита' 
 Order by datesales
 Limit 5
+
   #Запрос к 7 заданию  
-SELECT * FROM db_practice_5.cars;
 SELECT typepayment, avg(transactionamount) as transactionavg
 From cars
 WHERE typepayment = 'наличная'
-
-
 SELECT * FROM db_practice_5.cars;
 SELECT typepayment, avg(transactionamount) as transactionavg
 From cars
 WHERE typepayment = 'безналичная'
+
   #Запрос к 8 заданию  
-SELECT * FROM db_practice_5.cars;
 Select managersurname, sum(transactionamount) as allmoney
 From cars
 where managersurname = 'Иванов'
